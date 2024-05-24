b67d0d8fc394195e8da7480839572aa93388f823
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-05-23 21:30:50 -0400

    created matrix and tensor datatypes

---
+++ quasar/matrix/matrix.h
@@ -0,0 +1,8 @@
+typedef struct {
+    void* data;
+    int* shape;
+    int rank;
+    int dtype;
+    int size;
+
+} Matrix;
---
+++ quasar/tensor/tensor.h
@@ -0,0 +1,19 @@
+typedef enum {
+    DEVICE_CPU,
+    DEVICE_GPU
+    // Additional devices like DEVICE_TPU can be added here
+} DeviceType;
+
+typedef struct {
+    void* data;
+    int* shape;
+    int rank;
+    int dtype;
+    int size;
+    DeviceType device;
+} Tensor;
+
+
+void tensor_add(Tensor* a, Tensor* b, Tensor* result){
+
+}
